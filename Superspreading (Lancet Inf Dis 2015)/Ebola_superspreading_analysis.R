37b5c179f179466bda21e58ced88eab650524bef
Christian Althaus <christian.althaus@alumni.ethz.ch>
2015-07-09 23:21:04 +0200

    Published analyses

---
+++ Superspreading (Lancet Inf Dis 2015)/Ebola_superspreading_analysis.R
@@ -0,0 +1,54 @@
+library(fitdistrplus)
+
+# Number of individuals in the trees
+n <- 152
+# Number of secondary cases for all individuals
+c1 <- c(1,2,2,5,14,1,4,4,1,3,3,8,2,1,1,4,9,9,1,1,17,
+        2,1,1,1,4,3,3,4,2,5,1,2,2,1,9,1,3,1,2,1,1,2)
+c0 <- c(c1,rep(0,n-length(c1)))
+# Fitting a negative binomial distribution to the number of secondary cases
+fit.cases <- fitdist(c0,"nbinom")
+summary(fit.cases)
+plot(fit.cases)
+
+# Range of reported serial intervals
+days <- 0:43
+# Observed intervals for each day
+frequency <- c(0,1,3,1,4,1,6,1,2,2,11,6,0,1,10,3,5,8,4,3,3,1,
+               0,2,0,2,0,3,1,1,1,0,0,0,0,0,2,0,1,0,1,1,0,1)
+d <- rep(days,frequency)
+# Fitting a gamma distribution to the serial interval
+fit.serial <- fitdist(d,"gamma")
+summary(fit.serial)
+plot(fit.serial)
+
+# Set seed for random number generator
+set.seed(645)
+# Number of simulation runs
+runs <- 1e2
+# Number of initial cases
+seed <- 1
+# Initialize plot
+plot(NA,xlim=c(0,100),ylim=c(0,100),xlab="Time (days)",
+     ylab="Cumulative number of EVD cases",frame=FALSE)
+# Set color scheme for different trajectories
+cols <- sample(terrain.colors(runs))
+# Simulate outbreak trajectories
+for(i in 1:runs) {
+    cases <- seed
+	t <- rep(0,seed)
+	times <- t
+	while(cases > 0) {
+		secondary <- rnbinom(cases,size=fit.cases$estimate[1],mu=fit.cases$estimate[2])
+        t.new <- numeric()
+		for(j in 1:length(secondary)) {
+			t.new <- c(t.new,t[j] + rgamma(secondary[j],shape=fit.serial$estimate[1],
+                       rate=fit.serial$estimate[2]))
+		}
+		cases <- length(t.new)
+		t <- t.new
+		times <- c(times,t.new)
+	}
+	lines(sort(times),1:length(times),col=cols[i],lwd=1)
+	points(max(times),length(times),col=cols[i],pch=16)
+}
---
+++ West Africa (PLOS Curr 2014)/Ebola_outbreak_West_Africa_analysis.R
@@ -0,0 +1,110 @@
+###############################################################################
+#                                                                             #
+# ESTIMATING THE REPRODUCTION NUMBER OF EBOLA VIRUS (EBOV)                    #
+# DURING THE 2014 OUTBREAK IN WEST AFRICA                                     #
+#                                                                             #
+# REFERENCE: ALTHAUS CL. PLOS CURR. 2014 SEP 2.								  #
+# DOI: 10.1371/currents.outbreaks.91afb5e0f279e7f29e7056095255b288			  #
+#                                                                             #
+# Copyright (C) 2014 Christian L. Althaus                                     #
+# (christian.althaus@alumni.ethz.ch)                                          #
+#                                                                             #
+# This demo is free software; you can redistribute it and/or                  #
+# modify it under the terms of the GNU General Public License                 #
+# as published by the Free Software Foundation; either version 2              #
+# of the License, or (at your option) any later version.                      #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program; if not, write to the Free Software                 #
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. #
+#                                                                             #
+###############################################################################
+
+# Necessary libraries
+library(deSolve)
+library(chron)
+library(bbmle)
+
+# Read the data file
+ebola <- read.csv("data.csv")
+ebola$Date <- chron(as.character(ebola$Date),format=c(dates = "day mon year"))
+
+# Definition of the SEIR model
+SEIR <- function(t, x, parms) {
+	with(as.list(c(parms,x)),{
+		if(t < tau1) beta <- beta0
+		else beta <- beta0*exp(-k*(t-tau1))
+		N <- S + E + I + R
+		dS <- - beta*S*I/N
+		dE <- beta*S*I/N - sigma*E
+		dI <- sigma*E - gamma*I
+		dR <- (1-f)*gamma*I
+		dD <- f*gamma*I
+		dC <- sigma*E
+		der <- c(dS,dE,dI,dR,dD,dC)
+		list(der)
+	})
+}
+
+# Negative log-likelihood
+nll <- function(beta0,k,f,tau0,tau1,sigma,gamma) {
+	pars <- c(beta0=beta0,k=k,f=f,tau0=tau0,tau1=tau1,sigma=sigma,gamma=gamma)
+	pars <- trans(pars)
+	times <- c(0,data$times+pars["tau0"])
+	simulation <- as.data.frame(ode(init,times,SEIR,parms=pars))
+	simulation <- simulation[-1,]
+	ll <- sum(dpois(data$cases,simulation$C,log=TRUE)) + sum(dpois(data$deaths,simulation$D,log=TRUE))
+	return(-ll)
+}
+
+# Parameter transformation
+trans <- function(pars) {
+	pars["beta0"] <- exp(pars["beta0"])
+	pars["k"] <- exp(pars["k"])
+	pars["f"] <- plogis(pars["f"])
+	pars["tau0"] <- exp(pars["tau0"])
+	pars["tau1"] <- exp(pars["tau1"])
+	return(pars)
+}
+
+# GUINEA: Prepare the data, set the initial values and fit the model
+data <- na.omit(ebola[c("Date","Guinea_Cases","Guinea_Death")])
+names(data) <- c("times","cases","deaths")
+begin <- chron("2 Dec 2013", format=c(dates = "day mon year")) 
+delay <- as.numeric(data$times[1] - begin)
+data$times <- data$times - data$times[1]
+N <- 1e6		
+init <- c(S = N - 1, E = 0, I = 1, R = 0, D = 0, C = 1)
+fixed <- c(tau0 = log(delay), tau1 = -Inf, sigma = 1/5.3, gamma = 1/5.61)
+free <- c(beta0 = log(0.2), k = log(0.001), f = 0)
+fit <- mle2(nll,start=as.list(free),fixed=as.list(fixed),method="Nelder-Mead",control=list(maxit=1e3))
+trans(coef(fit))
+
+# SIERRA LEONE: Prepare the data, set the initial values and fit the model
+data <- na.omit(ebola[c("Date","SierraLeone_Cases","SierraLeone_Death")])
+names(data) <- c("times","cases","deaths")
+begin <- min(data$times) 
+data$times <- data$times - data$times[1]
+N <- 1e6		
+init <- c(S = N - 1, E = 0, I = 1, R = 0, D = 0, C = 1)
+fixed <- c(tau1 = -Inf, sigma = 1/5.3, gamma = 1/5.61)
+free <- c(beta0 = log(0.2), k = log(0.001), f = 0, tau0 = log(60))
+fit <- mle2(nll,start=as.list(free),fixed=as.list(fixed),method="Nelder-Mead",control=list(maxit=1e3))
+trans(coef(fit))
+
+# LIBERIA: Prepare the data, set the initial values and fit the model
+data <- na.omit(ebola[c("Date","Liberia_Cases","Liberia_Death")])
+names(data) <- c("times","cases","deaths")
+begin <- min(data$times) 
+data$times <- data$times - data$times[1]
+N <- 1e6		
+init <- c(S = N - 1, E = 0, I = 1, R = 0, D = 0, C = 1)
+fixed <- c(tau1 = -Inf, sigma = 1/5.3, gamma = 1/5.61, k = -Inf)
+free <- c(beta0 = log(0.2), f = 0, tau0 = log(60))
+fit <- mle2(nll,start=as.list(free),fixed=as.list(fixed),method="Nelder-Mead",control=list(maxit=2e3))
+trans(coef(fit))
