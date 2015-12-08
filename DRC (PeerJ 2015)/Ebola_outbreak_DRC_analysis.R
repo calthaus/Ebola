eef7fbe7920a63eb4addad05fe836bbfc7827863
Christian Althaus <christian.althaus@alumni.ethz.ch>
2015-12-08 23:17:21 +0100

    Updated journal name

--- DRC (PeerJ 2015)/Ebola_outbreak_DRC_analysis.R
+++ DRC (PeerJ 2015)/Ebola_outbreak_DRC_analysis.R
@@ -3,8 +3,8 @@
 #   RAPID DROP IN THE REPRODUCTION NUMBER DURING THE EBOLA OUTBREAK       #
 #   IN THE DEMOCRATIC REPUBLIC OF CONGO                                   #
 #                                                                         #
-#   REFERENCE: ALTHAUS CL. PEERJ PREPRINTS. 2015;3:E1279.                 #
-#   DOI: 10.7287/peerj.preprints.1041v1                                   #
+#   REFERENCE: ALTHAUS CL. PEERJ. 2015;3:e1418.	                		  #
+#   DOI: 10.7717/peerj.1418       				                          #
 #                                                                         #
 #   Copyright (C) 2015 by Christian L. Althaus                            #
 #   (christian.althaus@alumni.ethz.ch)                                    #
